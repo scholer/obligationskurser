cff0af73dbdd1cbe76908a660aa57b54fd68063b
Rasmus Scholer Sorensen <rasmusscholer@gmail.com>
2023-12-08 17:55:31 +0100

    Initial commit.

---
+++ scrape_jyskebank.py
@@ -0,0 +1,37 @@
+"""
+
+The page is protected by Cloudflare bot prevention.
+
+Use one of these packages:
+
+- https://pypi.org/project/cloudscraper/  (most up to date)
+- https://pypi.org/project/cfscrape/      (last relese 2020)
+
+"""
+from pathlib import Path
+import requests
+import time
+from datetime import datetime
+
+
+scrape_url = "https://www.jyskebank.dk/erhverv/ejendomsfinansiering/kurser"
+sleep_duration = 60*60  # 1 hour.
+
+user_agents = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+
+output_folder = "data/jyskebank_erhverv_kurser/scrape_raw"
+
+
+def main():
+    """ """
+    Path(output_folder).mkdir(exist_ok=True, parents=True)
+    res = requests.get(scrape_url, headers={"User-Agent": user_agents})
+    txt = res.text
+    fname = f"{datetime.now():%Y%m%d-%H%M%S}_{res.status_code}.html"
+    fpath = Path(output_folder) / fname
+    print(f"Writing {len(txt)} chars to file: {fpath}")
+    Path(fpath).write_text(txt)
+
+
+if __name__ == "__main__":
+    main()
