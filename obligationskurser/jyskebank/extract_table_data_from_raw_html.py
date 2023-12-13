9198c64f6a5630d083576cfee34b662364f2dbdb
Rasmus Scholer Sorensen <rasmusscholer@gmail.com>
2023-12-13 08:18:00 +0100

    Fixed type-checker errors

--- obligationskurser/jyskebank/extract_table_data_from_raw_html.py
+++ obligationskurser/jyskebank/extract_table_data_from_raw_html.py
@@ -1,6 +1,10 @@
 """
 Module for extracting table data from raw HTML pages to Parquet/CSV files.
 
+Usage:
+
+>>> python obligationskurser/jyskebank/extract_table_data_from_raw_html.py
+
 
 
 Libraries for parsing html:
@@ -36,6 +40,7 @@ def get_html_table_elem(html_str):
     soup = BeautifulSoup(html_str, features="html.parser")
     type(soup.find(class_=div_class))  # bs4.element.Tag
     div_elem = soup.find(class_=div_class)
+    assert div_elem is not None
     table_elem = div_elem.find("table")
     # table_elem = soup.find("table")
     return table_elem
@@ -45,9 +50,11 @@ def extract_df_from_html_file(html_file):
     """ """
     html_str = Path(html_file).read_text()
     table_elem = get_html_table_elem(html_str)
+    assert table_elem is not None
+    assert not isinstance(table_elem, int)
     # print(table_elem.decode())
     # Returns list of Pandas DataFrames. Depends on lxml
-    dfs = pd.read_html(io.StringIO(table_elem.decode()), decimal=",", thousands=".")  
+    dfs = pd.read_html(io.StringIO(table_elem.decode()), decimal=",", thousands=".")  # type: ignore
     return dfs[0]
 
 
@@ -78,7 +85,7 @@ def extract_and_save(html_file, output=(("parquet", "{html_dir}/../bs_pd_parquet
         # print(df)
 
 
-def find_new_html_files(html_dir, processed_file="_processed.txt"):
+def find_new_html_files(html_dir, processed_file: str | Path = "_processed.txt"):
     """ """
     html_files = Path(html_dir).glob("*_200.html")
     try:
