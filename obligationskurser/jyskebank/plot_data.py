3680a3ac4b1bdb053ca2527d2b30ad0d191d6744
Rasmus Scholer Sorensen <rasmusscholer@gmail.com>
2023-12-13 08:18:54 +0100

    Create more plots
    Tilbudskurs, aktuel kurs, and 4pct_411.E.OA.56.
    Also print more info.

--- obligationskurser/jyskebank/plot_data.py
+++ obligationskurser/jyskebank/plot_data.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import matplotlib
 from matplotlib import pyplot
+import seaborn
 import pandas as pd
 
 
@@ -30,23 +31,61 @@ def main():
     parquet_basedir = "data/jyskebank_erhverv_kurser/bs_pd_parquet"
 
     combined_df = get_combined_df(parquet_basedir)
-    print(combined_df.columns)
+
+    print("Columns:")
+    print(combined_df.columns.values)
+
+    # Remove 
+    combined_df = combined_df[~combined_df["Løbetid/kuponrente"].str.startswith(r"30 år 1,00 %")]
+    print("Løbetid/kuponrente, unikke værdier:")
+    print("\n".join(sorted(combined_df["Løbetid/kuponrente"].unique())))
+
+    # Plot all columns for "30 år 5,00 % afdragsfri* 411.E.OA.56"
     selected_df = combined_df[combined_df["Løbetid/kuponrente"].str.startswith(r"30 år 5,00 % afdragsfri* 411.E.OA.56")]
-    print(selected_df)
-
+    # print(selected_df)
     fig, ax = pyplot.subplots(figsize=(20, 10))
-    plotax = selected_df.set_index("timestamp").plot(ax=ax)
-
+    selected_df.set_index("timestamp").plot(ax=ax)
     # ax.legend(loc='upper left')
     ax.legend(bbox_to_anchor=(1.25, 1.0))
     fig.tight_layout()
-
     plotfn = "5pct_411.E.OA.56.png"
     print("Saving plot to file:", plotfn)
-    plotax.figure.savefig(plotfn)
+    fig.savefig(plotfn)
+
+    # Plot all columns for "30 år 5,00 % afdragsfri* 411.E.OA.56"
+    selected_df = combined_df[combined_df["Løbetid/kuponrente"].str.startswith(r"30 år 4,00 % afdragsfri* 411.E.OA.56")]
+    # print(selected_df)
+    fig, ax = pyplot.subplots(figsize=(20, 10))
+    selected_df.set_index("timestamp").plot(ax=ax)
+    # ax.legend(loc='upper left')
+    ax.legend(bbox_to_anchor=(1.25, 1.0))
+    fig.tight_layout()
+    plotfn = "4pct_411.E.OA.56.png"
+    print("Saving plot to file:", plotfn)
+    fig.savefig(plotfn)
+
+    # Plot all rows for a specific column ('Tilbudskurs'):
+    fig, ax = pyplot.subplots(figsize=(15, 10))
+    seaborn.lineplot(data=combined_df, x="timestamp", y="Tilbudskurs", hue="Løbetid/kuponrente")
+    ax.legend(bbox_to_anchor=(1.0, 1.0))
+    fig.tight_layout()
+    plotfn = 'Tilbudskurs.png'
+    print("Saving plot to file:", plotfn)
+    fig.savefig(plotfn)
+
+    # Plot all rows for a specific column ('Aktuel kurs'):
+    fig, ax = pyplot.subplots(figsize=(15, 10))
+    seaborn.lineplot(data=combined_df, x="timestamp", y="Aktuel kurs", hue="Løbetid/kuponrente")
+    ax.legend(bbox_to_anchor=(1.0, 1.0))
+    fig.tight_layout()
+    plotfn = 'Aktuel_kurs.png'
+    print("Saving plot to file:", plotfn)
+    fig.savefig(plotfn)
+
+    # Plot all "Udbetalingskurs Fastkursaftale":
+    fastkurs_cols = [col for col in combined_df.columns.values if "Udbetalingskurs Fastkursaftale" in col]
 
 
-# help(pd.Series.str)
 
 if __name__ == "__main__":
     main()
